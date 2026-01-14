49c19e28f23cc5a29d1dcd647ae066379329d6e8
Victoria <130781096+Vico26@users.noreply.github.com>
2026-01-13 20:04:06 -0600

    Inicio del proyecto
    Se empieza la parte de la creación del serverPrincipal.js y la parte de prueba para la geolocalización también.

---
+++ serverPrincipal.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.post('/location', (req, res) => {
+    const { latitude, longitude } = req.body;
+    console.log(`Ubicación recibida: Latitud ${latitude}, Longitud ${longitude}`);
+    res.json({
+        message: 'Ubicación recibida correctamente',
+        latitude,
+        longitude
+    });
+});
+
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.listen(port, () => {
+    console.log(`Servidor escuchando en http://localhost:${port}`);
+}); 
